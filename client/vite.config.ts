411c6ceb5b729f7e8546224a7f87e605998511ca
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-03-09 17:07:37 +0900

    [update] ビルド設定

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"./",
+  build: {
+    outDir: "../docs",
+    assetsDir: "./assets"
+  },
   plugins: [vue()]
 })
